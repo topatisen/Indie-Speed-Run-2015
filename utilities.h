88811d9d08b1ccac8c52de5c3186dd16c3af7e39
Gabriel Fornaeus <gf@hax0r.se>
2015-10-10 18:51:59 +0200

    New random

--- utilities.h
+++ utilities.h
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <iostream>
+#include <random>
 using namespace std;
 
 #ifndef UTIL_H
@@ -9,4 +10,5 @@ void logSDLError(std::ostream &os, const std::string &msg);
 SDL_Texture *loadTexture(const std::string &file, SDL_Renderer *ren);
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y);
 SDL_Texture *createTextMessage(TTF_Font *font, SDL_Color color, const char *message, SDL_Renderer *ren);
+unsigned long pRand(void);
 #endif
