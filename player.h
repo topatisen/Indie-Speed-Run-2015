b34e15e2994c10f6f14829632a64e0f3b66e9554
Johannes Fornaeus <johannes.fornaeus@gmail.com>
2015-10-10 11:09:54 +0200

    Added player-header-file

---
+++ player.h
@@ -0,0 +1,24 @@
+//Player Header
+#ifndef PLAYER_H
+#define PLAYER_H
+
+class cPlayer
+{
+	public:
+	float x, y, vspeed, hspeed;
+	
+	void create()
+	{
+		//Create Code
+	}
+	void run()
+	{
+		//Logic code
+	}
+	void draw(SDL_Renderer *ren, SDL_Texture *sPlayer)
+	{
+		//Draw Code
+	}
+};
+
+#endif
